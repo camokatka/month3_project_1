44388aed1d2686f8894500baa190324161c50426
camokatka <elizaveta.serykh@gmail.com>
2023-08-01 23:15:27 +0300

    без док заданий

---
+++ month3_project_1/FinalViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  month3_project_1
+//
+//  Created by Elizabeth Serykh on 01.08.2023.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- month3_project_1/ViewController.swift
+++ month3_project_1/ViewController.swift
@@ -9,9 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var IdTextField: UITextField!
+    
+    
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+    
+    @IBOutlet weak var SingInButton: UIButton!
+    
+    @IBAction func signIn(_ sender: Any) {
+        var flag = false
+        if IdTextField.text?.isEmpty ?? true {
+            IdTextField.layer.borderColor = UIColor.red.cgColor
+            IdTextField.layer.borderWidth = 1
+            IdTextField.placeholder = "Fill  the information"
+        } else {
+            IdTextField.layer.borderColor = UIColor.white.cgColor
+            flag = true
+        }
+        
+        if PasswordTextField.text?.isEmpty ?? true {
+            PasswordTextField.layer.borderColor = UIColor.red.cgColor
+            PasswordTextField.layer.borderWidth = 1
+            PasswordTextField.placeholder = "Fill the information"
+
+        } else {
+            PasswordTextField.layer.borderColor = UIColor.white.cgColor
+            flag = true
+        }
+        
+        if flag {
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "FinalViewController") as! FinalViewController
+            present(vc, animated: true)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
